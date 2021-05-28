a7689b00657ad898d5a6de01dba9474ccf34e350
maguiamaya <eugenia.amaya@unc.edu.ar>
2021-05-28 15:28:39 -0300

    configura servidor

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // npm init -y ...npm i express... despues creamos app.js
 const express = require("express");
+const port = 3000;
 
 //console.log(express); // solo para ver q tenia express no es muy necesario
 
 const app = express();
 // estas 2 lineas siempre van!
-app.listen(3000, () => console.log("servidor funcionando en el puerto 3000"),  ) //puerto, callback arrow function, 
+app.listen(port, () => console.log("servidor funcionando en el puerto " + port)) //puerto, callback arrow function, 
